7f0a21aa1c8f0a546c6fd138e6d64e6fbc44d57c
Day Meijroos <pekieend@gmail.com>
2023-04-06 10:14:38 +0200

    🛠️: Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import TrpcExample from '../components/example/trpc.example'
 import AuthExample from '../components/example/auth.example'
 import LoginExample from '../components/example/login.example'
-import Separator from '../components/aria/seperator'
 
 export default function Home() {
   return (
-    <div className="flex flex-row justify-around min-h-screen bg-amber-200 items-center">
+    <div className="flex flex-col lg:flex-row justify-around min-h-screen bg-amber-200 items-center">
       <div className="flex flex-col gap-2 w-72">
         <h1 className="text-6xl font-bold text-blue-800">
           Welcome to <a href="https://nextjs.org" className="text-blue-600 underline">Next.js!</a>
